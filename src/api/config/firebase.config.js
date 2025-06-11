5e53b15c3ab08568ac9ce82b0300c45b00a63fb8
wanda azhar <wandaazhar@gmail.com>
2025-06-11 17:21:13 -0600

    Adding config/firebase.config.js for firebase connection and configuration

---
+++ src/api/config/firebase.config.js
@@ -0,0 +1,15 @@
+const admin = require('firebase-admin');
+
+// IMPORTANT: Path to your Firebase Service Account Key JSON file.
+// We will get this file from the Firebase Console in the next steps.
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+console.log('Firebase Admin SDK initialized successfully.');
+
+module.exports = { db };
