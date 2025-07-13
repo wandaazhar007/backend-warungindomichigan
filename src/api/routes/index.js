e782d61272e5c4888d23086af9914117f1d1c80c
wanda azhar <wandaazhar@gmail.com>
2025-07-13 01:06:17 -0600

    add categories module

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const productRoutes = require('./product.routes');
 const orderRoutes = require('./order.routes');
-const customerRoutes = require('./customer.routes'); // Assuming customerRoutes is defined in customer.routes.js
+const customerRoutes = require('./customer.routes');
 const contactRoutes = require('./contact.routes');
+const categoryRoutes = require('./category.routes');
 
 const router = express.Router();
 
 router.use('/products', productRoutes);
 router.use('/orders', orderRoutes);
-router.use('/customers', customerRoutes); // Assuming customerRoutes is defined in customer.routes.js
+router.use('/customers', customerRoutes);
 router.use('/contact', contactRoutes);
+router.use('/categories', categoryRoutes);
 
 module.exports = router;
